4b22ed23ba51963eb229be98353302143a1dee78
NicolasVerone <nvpro@email.com>
2021-04-22 21:52:35 +0200

    fin projet

--- js/form.js
+++ js/form.js
@@ -21,16 +21,12 @@ const formInit = () => {
     allInputs[3].addEventListener("input", () => {
         birthdateFieldCheck();
     });
-
     allInputs[4].addEventListener("input", () => {
         tournamentFieldCheck();
-
     });
     locationButton.forEach((radio) => radio.addEventListener("click", () => {
         locationButtonCheck();
-
     }));
-
     checkboxTc.addEventListener("change", () => {
         checkboxTcCheck();
     });
@@ -39,38 +35,43 @@ const formInit = () => {
     });
 };
 
+
 // fonction vérification champ prénom
 const nameFieldCheck = () => {
     const nameInput = document.getElementById("first");
     const nameInputOk = /^[a-zA-ZÀ-ÖØ-öø-ÿ -]+$/;
-    let nameError = document.getElementById("errorfirst");
+    const nameError = document.getElementById("errorfirst");
     // vérification changement attribut du champ par l'utilisateur
     if (nameInput.type != "text") {
-        ameInput.disabled = true;
+        nameInput.disabled = true;
         return false;
     }
     // vérification valeur vide
     if (nameInput.value.trim() === "") {
         nameError.innerHTML = "Ce champ doit être rempli si vous souhaitez vous inscrire";
         nameError.style.color = "orange";
+        nameInput.style.border = "2px solid orange";
         return false;
     }
     // vérification si longueur de valeur insuffisante
     if (nameInput.value.length < 2) {
         nameError.innerHTML = "Veuillez entrer 2 caractères ou plus pour ce champ";
         nameError.style.color = "orange"
+        nameInput.style.border = "2px solid orange";
         return false;
     }
     // vérification si valeur correpond à la regex servant de référence
     if (!nameInputOk.test(nameInput.value)) {
         nameError.innerHTML = "Ce champ ne peut contenir que des lettres et des '-'";
         nameError.style.color = "red";
+        nameInput.style.border = "2px solid red";
         return false;
     }
     // valeur OK
-        nameError.innerHTML = "Merci, ce champ est correctement rempli !";
-        nameError.style.color = "green";
-        return true
+    nameError.innerHTML = "Merci, ce champ est correctement rempli !";
+    nameError.style.color = "green";
+    nameInput.style.border = "2px solid green";
+    return true
 }
 
 // fonction vérification champ prénom
@@ -81,31 +82,37 @@ const surnameFieldCheck = () => {
     // vérification changement attribut du champ par l'utilisateur
     if (surnameInput.type != "text") {
         surnameInput.disabled = true;
+        surnameInput.style.border = "2px solid orange";
         return false;
     }
     // vérification valeur vide
     if (surnameInput.value.trim() === "") {
         surnameError.innerHTML = "Ce champ doit être rempli si vous souhaitez vous inscrire";
         surnameError.style.color = "orange";
+        surnameInput.style.border = "2px solid orange";
         return false;
     }
     // vérification si longueur de valeur insuffisante
     if (surnameInput.value.length < 2) {
         surnameError.innerHTML = "Veuillez entrer 2 caractères ou plus pour ce champ";
         surnameError.style.color = "orange"
+        surnameInput.style.border = "2px solid orange";
         return false;
     }
     // vérification si valeur correpond à la regex servant de référence
     if (!surnameInputOk.test(surnameInput.value)) {
         surnameError.innerHTML = "Ce champ ne peut contenir que des lettres et des '-'";
         surnameError.style.color = "red";
+        surnameInput.style.border = "2px solid red";
         return false;
     }
     // valeur OK
-        surnameError.innerHTML = "Merci, ce champ est correctement rempli !";
-        surnameError.style.color = "green";
-        return true
+    surnameError.innerHTML = "Merci, ce champ est correctement rempli !";
+    surnameError.style.color = "green";
+    surnameInput.style.border = "2px solid green";
+    return true
 }
+
 
 // fonction vérification champ email
 const emailFieldCheck = () => {
@@ -121,23 +128,27 @@ const emailFieldCheck = () => {
     if (emailInput.value.trim() === "") {
         emailError.innerHTML = "Ce champ doit être rempli si vous souhaitez vous inscrire"
         emailError.style.color = "orange";
+        emailInput.style.border = "2px solid orange";
         return false;
     }
     // vérification si valeur correpond à la regex servant de référence
     if (!emailInputOk.test(emailInput.value)) {
         emailError.innerHTML = "Ce champ doit contenir une adresse email valide";;
         emailError.style.color = "orange";
+        emailInput.style.border = "2px solid orange";
         return false;
     }
     // valeur OK
-        emailError.innerHTML = "Merci, ce champ est correctement rempli !";
-        emailError.style.color = "green";
-        return true;
+    emailError.innerHTML = "Merci, ce champ est correctement rempli !";
+    emailError.style.color = "green";
+    emailInput.style.border = "2px solid green";
+    return true;
 }
 
 // vérification du champ date de naissance
 const birthdateFieldCheck = () => {
-    const birthdateInput = document.getElementById("birthdate"); birthdateInput.maxlength = 10;
+    const birthdateInput = document.getElementById("birthdate");
+    birthdateInput.maxlength = 10;
     const birthdateError = document.getElementById("errorbirthdate");
     const birthdateFieldInputMax = "2003-05-01";
     const birthdateFieldInputMin = "1900-01-01";
@@ -150,22 +161,27 @@ const birthdateFieldCheck = () => {
     if (birthdateInput.value.trim() === "") {
         birthdateError.innerHTML = "Vous devez entrer votre date de naissance";
         birthdateError.style.color = "orange";
+        birthdateInput.style.border = "2px solid orange";
         return false;
     }
-    if (birthdateInput.value >= birthdateFieldInputMax || birthdateInput.value < birthdateFieldInputMin) {
-        birthdateError.innerHTML = "Votre date de naissance ne vous permet pas de vous inscrire";;
+    if (birthdateInput.value >= birthdateFieldInputMax
+        || birthdateInput.value < birthdateFieldInputMin) {
+        birthdateError.innerHTML = "Votre date de naissance ne vous permet pas de vous inscrire";
         birthdateError.style.color = "orange";
+        birthdateInput.style.border = "2px solid orange";
         return false;
     }
     if (birthdateInput.value.length > 10) {
         birthdateError.innerHTML = "Veuillez saisir une année valide";
-        birthdateError.style.color = "orange";
+        birthdateError.style.color = "red";
+        birthdateInput.style.border = "2px solid red";
         return false;
     }
     // valeur OK
-        birthdateError.innerHTML = "Merci, ce champ est correctement rempli !";;
-        birthdateError.style.color = "green";
-        return true;
+    birthdateError.innerHTML = "Merci, ce champ est correctement rempli !";
+    birthdateError.style.color = "green";
+    birthdateInput.style.border = "2px solid green";
+    return true;
 }
 
 // vérification du champ nombre de tournois
@@ -174,39 +190,43 @@ const tournamentFieldCheck = () => {
     const tournamentError = document.getElementById("errorquantity");
     // vérification changement attribut du champ par l'utilisateur
     if (tournamentInput.type !== "number") {
-        allInputs[4].disabled = true;
+        tournamentInput.disabled = true;
         return false;
     }
     // vérification de la valeur entrée par l'utilsateur (entre 0 et 6)
     if (tournamentInput.value < 0 || tournamentInput.value > 6) {
         tournamentError.innerHTML = "Ce champ doit contenir une valeur comprise entre 0 et 6"
         tournamentError.style.color = "red";
+        tournamentInput.style.border = "2px solid red";
         return false;
     }
     // vérification de l'absence de valeur entrée de l'utilisateur
     if (tournamentInput.value === "") {
         tournamentError.innerHTML = "Ce champ doit être rempli si vous souhaitez vous inscrire";
         tournamentError.style.color = "orange";
+        tournamentInput.style.border = "2px solid orange";
         return false;
     }
     // valeur OK
-        tournamentError.innerHTML = "Merci, ce champ est correctement rempli !";;
-        tournamentError.style.color = "green";
-        return true;
+    tournamentError.innerHTML = "Merci, ce champ est correctement rempli !";;
+    tournamentError.style.color = "green";
+    tournamentInput.style.border = "2px solid green";
+    return true;
 }
+
 // vérification des boutons radios ville
 const locationButtonCheck = () => {
     const locationCheckBox = document.getElementsByName("location");
     const locationError = document.getElementById("errorlocation");
-    // création d'une boucle vérifiant si un des boutons radio est coché
-    for (let i = 0; i < locationCheckBox.length; i++) {
-        if (locationCheckBox[i].checked) {
-            locationError.innerHTML = "Merci pour votre réponse";;
-            locationError.style.color = "green";
-            return true;
-        }
-        // bouton bien cochée 
-    }       return false;
+    // ccondition si l'un des boutons est coché
+    if (locationCheckBox[0].checked ||
+        locationCheckBox[1].checked ||
+        locationCheckBox[2].checked ||
+        locationCheckBox[3].checked) {
+        locationError.innerHTML = "Merci pour votre réponse !";
+        locationError.style.color = "green";
+        return true
+    }
 }
 
 // vérification ddes conditions générales bien cochés
@@ -215,20 +235,20 @@ const checkboxTcCheck = () => {
     const checkboxTcError = document.getElementById("errorcheckbox");
     // si les conditions générales ne sont pas cochées
     if (!checkboxTc.checked) {
-        checkboxTcError .innerHTML = "Vous devez vérifier que vous acceptez les termes et conditions.";
-        checkboxTcError .style.color = 'orange';
+        checkboxTcError.innerHTML = "Vous devez vérifier que vous acceptez les termes et conditions.";
+        checkboxTcError.style.color = 'orange';
         return false;
     }
     // si OK
-        checkboxTcError .innerHTML = " ";
-        return true;
-
-}// Fonction validation du formulaire
+    checkboxTcError.innerHTML = " ";
+    return true;
+}
+// Fonction validation du formulaire
 const formSubmission = (event) => {
     const errorSubmit = document.getElementById("messagesubmit");
-    const locationCheckBoxError = () => { locationCheckBox.innerHTML = "Vous devez choisir une option" };
-    const locationCheckBox = document.getElementById("errorlocation");
     errorSubmit.style.color = 'orange';
+    const locationCheckBox = document.getElementById("errorlocation");
+    const locationCheckBoxError = () => { locationCheckBox.innerHTML = "Vous devez choisir une option" };
     const errorSubmitMessage = () => { errorSubmit.innerHTML = "Veuillez remplir tous les champs pour valider votre inscription" };
 
     if (nameFieldCheck() !== true) {
@@ -250,7 +270,6 @@ const formSubmission = (event) => {
         event.preventDefault();
         errorSubmitMessage();
         return false;
-
     }
     if (tournamentFieldCheck() !== true) {
         event.preventDefault();
@@ -269,7 +288,7 @@ const formSubmission = (event) => {
         errorSubmitMessage();
         return false;
     }
-    location.href = "validation.html";
+    document.getElementById('form').action = "validation.html"
 };
 
 formInit();
